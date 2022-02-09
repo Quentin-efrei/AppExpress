89021a15db090f2ec9a260fd37cfbfe8df5f339c
quentin courtois <quentin.courtois@efrei.net>
2022-02-09 12:05:56 +0100

    Test releease

---
+++ backend/index.js
@@ -0,0 +1,36 @@
+// Constants
+const PORT = process.env.PORT || 8000;
+const session = require('express-session');
+const express = require("express");
+var cors = require("cors");
+// App
+const app = express();
+
+app.use(session({
+            secret: 'secret string',
+            resave : false,
+            saveUninitialized : false,
+            cookie : {/* can add cookie related info here*/}
+        })
+      );
+
+app.use(cors({
+    origin: 'http://localhost:3000',
+    methods: ['POST', 'PUT', 'GET', 'OPTIONS', 'HEAD'],
+    credentials: true
+}));
+
+app.get('/', function(req,res){
+    if (!req.session.pageCountByCurrentUserOfAnyNameYouWant)
+        req.session.pageCountByCurrentUserOfAnyNameYouWant = 0;
+    req.session.pageCountByCurrentUserOfAnyNameYouWant++;
+    res.send({
+        pageCount : req.session.pageCountByCurrentUserOfAnyNameYouWant
+    });
+});
+
+/*app.get('/', function(req, res, next) {
+    res.send('API is working properly');
+});*/
+
+app.listen(PORT, () => console.log(`The server is started on http://localhost:${PORT}`));
